976e7cdf435db971bffa9ac5e4949dccf8d40c2f
Donovan LaDuke <dladukedev@gmail.com>
2025-07-28 22:10:33 -0400

    add inland47 mappings and fix bad chocofi remap

--- keyboards/inland/mk47/keymaps/dladukedev/keymap.c
+++ keyboards/inland/mk47/keymaps/dladukedev/keymap.c
@@ -1,8 +1,8 @@
 #include QMK_KEYBOARD_H
 #include <stdio.h>
 
-// INSTALL COMMAND FOR CHOCOFI
-// qmk flash -kb chocofi -km dladukedev -e CONVERT_TO=promicro_rp2040
+// INSTALL COMMAND FOR INLAND 47
+// qmk flash -kb inland -km dladukedev
 
 // KEYMAP
 enum layers {
@@ -13,32 +13,32 @@ enum layers {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_BASE] = LAYOUT_split_3x5_3(
-        LGUI_T(KC_Q),    KC_W,    KC_E,    KC_R,    KC_T,                                    KC_Y,            KC_U,    KC_I,    KC_O,    RGUI_T(KC_SCLN),
-        KC_A,            KC_S,    KC_D,    KC_F,    KC_G,                                    KC_H,            KC_J,    KC_K,    KC_L,    KC_P,
-        LALT_T(KC_Z),    KC_X,    KC_C,    KC_V,    KC_B,                                    KC_N,            KC_M,    KC_COMM, KC_DOT,  RALT_T(KC_QUOT),
-                                           KC_NO,   KC_RSFT,  MO(_NUMBER),        MO(_NAV),  RCTL_T(KC_SPC),  KC_NO
+    [_BASE] = LAYOUT_planck_mit(
+        KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,         KC_T,       KC_Y,    KC_U,      KC_I,    KC_O,    KC_SCLN,  KC_BSPC,
+        KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,         KC_G,       KC_H,    KC_J,      KC_K,    KC_L,    KC_P,     KC_ENT,
+        KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,         KC_B,       KC_N,    KC_M,      KC_COMM, KC_DOT,  KC_QUOT,  KC_DEL,
+        KC_LGUI,   KC_NO,   KC_NO,   KC_NO,   MO(_NUMBER),       KC_SPC,          MO(_NAV),  KC_LEFT, KC_DOWN, KC_UP,    KC_RGHT
     ),
 
-    [_COLEMAK] = LAYOUT_split_3x5_3(
-        LGUI_T(KC_Q),    KC_W,    KC_F,    KC_P,    KC_B,                                 KC_J,     KC_L,    KC_U,    KC_O,    RGUI_T(KC_SCLN),
-        KC_A,            KC_R,    KC_S,    KC_T,    KC_G,                                 KC_M,     KC_N,    KC_E,    KC_I,    KC_O,
-        LALT_T(KC_Z),    KC_X,    KC_C,    KC_D,    KC_V,                                 KC_K,     KC_H,    KC_COMM, KC_DOT,  RALT_T(KC_QUOT),
-                                           KC_TRNS, KC_TRNS, KC_TRNS,           KC_TRNS,  KC_TRNS,  KC_TRNS
+    [_COLEMAK] = LAYOUT_planck_mit(
+        KC_TRNS, KC_Q,    KC_W,    KC_F,    KC_P,          KC_B,      KC_J,     KC_L,     KC_U,    KC_O,    KC_SCLN, KC_TRNS,
+        KC_TRNS, KC_A,    KC_R,    KC_S,    KC_T,          KC_G,      KC_M,     KC_N,     KC_E,    KC_I,    KC_O,    KC_TRNS,
+        KC_TRNS, KC_Z,    KC_X,    KC_C,    KC_D,          KC_V,      KC_K,     KC_H,     KC_COMM, KC_DOT,  KC_QUOT, KC_TRNS,
+        KC_TRNS, KC_NO,   KC_NO,   KC_NO,   MO(_NUMBER),       KC_SPC,          MO(_NAV),  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
 
-    [_NUMBER] = LAYOUT_split_3x5_3(
-        KC_ESC,  KC_F1,    KC_F2,    KC_F3,    KC_F4,                           KC_COMM,  KC_7,    KC_8,   KC_9,    KC_BSPC,
-        KC_TAB,    KC_F5,    KC_F6,    KC_F7,    KC_F8,                           KC_BSPC,  KC_4,    KC_5,   KC_6,    KC_ENT,
-        KC_NO,    KC_F9,    KC_F10,   KC_F11,   KC_F12,                          KC_DOT,   KC_1,    KC_2,   KC_3,    KC_DEL,
-                                     KC_NO,    KC_NO,   KC_TRNS,          KC_TRNS,  KC_0,     KC_NO
+    [_NUMBER] = LAYOUT_planck_mit(
+        KC_TRNS, KC_NO,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_NO,   KC_7,    KC_8,    KC_9,    KC_NO,   KC_TRNS,
+        KC_TRNS, KC_NO,   KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_NO,   KC_4,    KC_5,    KC_7,    KC_NO,   KC_TRNS,
+        KC_TRNS, KC_NO,   KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_NO,   KC_1,    KC_2,    KC_3,    KC_NO,   KC_TRNS,
+        KC_TRNS, KC_NO,   KC_NO,    KC_NO,    KC_NO,         KC_SPC,        KC_0,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
 
-    [_NAV] = LAYOUT_split_3x5_3(
-        KC_ESC,  MS_WHLL,    MS_UP,     MS_WHLR,   MS_WHLU,                             KC_PGUP,   KC_HOME,    TG(_COLEMAK),    KC_END,     KC_BSPC,
-        KC_TAB,    MS_LEFT,    MS_DOWN,   MS_RGHT,   MS_WHLD,                             KC_LEFT,   KC_DOWN,    KC_UP,    KC_RGHT,    KC_ENT,
-        KC_NO,    KC_MPRV,    KC_MPLY,   KC_MNXT,   MS_BTN3,                             KC_PGDN,   KC_VOLU,    KC_MUTE,  KC_VOLD,    KC_DEL,
-                                        KC_NO,     MS_BTN1,   MS_BTN2,          KC_TRNS,  KC_NO,    KC_NO
+    [_NAV] = LAYOUT_planck_mit(
+        KC_TRNS, KC_NO,    MS_WHLL,    MS_UP,     MS_WHLR,   MS_WHLU,    KC_PGUP,   KC_HOME,    TG(_COLEMAK),  KC_END,   KC_NO,   KC_TRNS,
+        KC_TRNS, KC_NO,    MS_LEFT,    MS_DOWN,   MS_RGHT,   MS_WHLD,    KC_LEFT,   KC_DOWN,    KC_UP,         KC_RGHT,  KC_NO,   KC_TRNS,
+        KC_TRNS, KC_NO,    KC_MPRV,    KC_MPLY,   KC_MNXT,   MS_BTN3,    KC_PGDN,   KC_VOLU,    KC_MUTE,       KC_VOLD,  KC_NO,   KC_TRNS,
+        KC_TRNS, KC_NO,    KC_NO,      KC_NO,     MS_BTN1,         MS_BTN2,         KC_NO,      KC_NO,         KC_NO,    KC_NO,   QK_BOOT
     )
 };
 
@@ -134,18 +134,3 @@ combo_t key_combos[] = {
   [GRV] = COMBO(grv_combo, KC_GRV)
 };
 
-// KEY OVERRIDES
-const key_override_t q_esc_key_override = ko_make_basic(MOD_MASK_CTRL, LGUI_T(KC_Q), KC_ESC);
-const key_override_t h_bksp_key_override = ko_make_with_layers(MOD_MASK_CTRL, KC_H, KC_BSPC, 1<<_BASE);
-const key_override_t j_entr_key_override = ko_make_with_layers(MOD_MASK_CTRL, KC_J, KC_ENT, 1<<_BASE);
-const key_override_t m_bksp_key_override = ko_make_with_layers(MOD_MASK_CTRL, KC_M, KC_BSPC, 1<<_COLEMAK);
-const key_override_t n_entr_key_override = ko_make_with_layers(MOD_MASK_CTRL, KC_N, KC_ENT, 1<<_COLEMAK);
-
-// This globally defines all key overrides to be used
-const key_override_t *key_overrides[] = {
-	&q_esc_key_override,
-	&h_bksp_key_override,
-	&j_entr_key_override,
-	&m_bksp_key_override,
-	&n_entr_key_override
-};
